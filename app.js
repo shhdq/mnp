bde1d8b5f91cc98a3e191d510c94322a33f9975d
Madars B <balodis.madars@gmail.com>
2020-08-09 23:28:38 +0300

    test

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
 const cookieContainer = document.querySelector(".cookie-container");
 const cookieButton = document.querySelector(".cookie-btn");
-const checkbox1 = document.querySelector("input[name=checkbox1]");
-const checkbox2 = document.querySelector("input[name=checkbox2]");
-
-checkbox1.addEventListener("change", function () {
-  if (this.checked) {
-    localStorage.setItem("chechbox", "true");
-  } else {
-    alert("Neapstiprinot datu apstrādi, mēs nevarēsim ar jums sazināties!");
-  }
-});
-
-checkbox2.addEventListener("change", function () {
-  if (this.checked) {
-    localStorage.setItem("chechbox", "true");
-  } else {
-    alert("Neapstiprinot datu apstrādi, mēs nevarēsim ar jums sazināties!");
-  }
-});
 
 cookieButton.addEventListener("click", () => {
   cookieContainer.classList.remove("active");
